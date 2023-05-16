5507fdef71ab8e2fdf6e8fe3875ccc33844429a4
Boas Pucker <36674497+bpucker@users.noreply.github.com>
2023-05-16 06:17:57 +0200

    Update run_smudgeplot.sh

--- run_smudgeplot.sh
+++ run_smudgeplot.sh
@@ -2,6 +2,6 @@
 echo "/vol/biotools/bin/jellyfish dump -c -L 31 -U 10000 \
 /./20200214_genome_size_estimation/genome_size_data/fw_19/reads.jf \
 | /./bin/smudgeplot/exec/smudgeplot.py hetkmers -o /./20200219_smudgeplot/kmer_pairs_19" \
-| qsub -cwd -N smudge -l vf=200G -l arch=lx-amd64 -P rapresmabs -pe multislot 1 \
+| qsub -cwd -N smudge -l vf=200G -l arch=lx-amd64 -P xxx -pe multislot 1 \
 -o /./20200219_smudgeplot/output.txt \
 -e /./20200219_smudgeplot/error.txt
